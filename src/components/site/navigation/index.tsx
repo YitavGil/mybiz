8cd934f842907da3bc6bbc6dc364d540a54c266a
YitavGil <yitavga@yahoo.com>
2024-05-19 11:15:24 +0300

    fixed login redirect

--- src/components/site/navigation/index.tsx
+++ src/components/site/navigation/index.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 const Navigation = ({ user }: Props) => {
   return (
-    <div className="p-4 flex items-center justify-between relative">
+    <div className="fixed top-0 right-0 left-0 p-4 flex items-center justify-between z-10">
       <aside className="flex items-center gap-2">
         <Image
           src={"./assets/plura-logo.svg"}
@@ -30,7 +30,7 @@ const Navigation = ({ user }: Props) => {
       </nav>
       <aside className="flex gap-2 items-center">
       <Link
-          href={'/agency/sign-in'}
+          href={'/agency'}
           className="bg-primary text-white p-2 px-4 rounded-md hover:bg-primary/80"
         >
           Login
