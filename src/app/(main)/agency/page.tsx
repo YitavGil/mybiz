7e74f9a7ac885c7de2ba0e37891e508d6d615940
YitavGil <yitavga@yahoo.com>
2024-05-26 14:08:27 +0300

    verifyAndAcceptinvitation complete

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,20 +1,36 @@
-import { getAuthUserDeatils } from '@/lib/queries';
-import { currentUser } from '@clerk/nextjs';
-import { redirect } from 'next/navigation';
-import React from 'react'
+import { getAuthUserDeatils, verifyAndAcceptinvitation } from "@/lib/queries";
+import { currentUser } from "@clerk/nextjs";
+import { Plan } from "@prisma/client";
+import { redirect } from "next/navigation";
+import React from "react";
 
-const Page = async () => {
-  const authUser = await currentUser();
-  if(!authUser) return redirect("/sign-in");
+const Page = async ({
+  searchParams,
+}: {
+  searchParams: { plan: Plan; state: string; code: string };
+}) => {
+  const agencyId = await verifyAndAcceptinvitation();
 
-  const agencyId = await verifyAndAcceptinvitation() 
+  //get user details
 
-    //get user details
+  const user = await getAuthUserDeatils();
+  if (agencyId) {
+    if (user?.role === "SUBACCOUNT_GUEST" || user?.role === "SUBACCOUNT_USER") {
+      return redirect("/subaccount");
+    } else if (user?.role === "AGENCY_OWNER" || user?.role === "AGENCY_ADMIN") {
+      if(searchParams.plan) {
+        return redirect(`/agency/${agencyId}/billing?plan=${searchParams.plan}`)
+      }
 
-    const user = getAuthUserDeatils()
-  return (
-    <div>Agency</div>
-  )
-}
+      if(searchParams.state){
+        const statePath = searchParams.state.split('___')[0];
+        const stateAgencyId = searchParams.state.split('___')[1];
 
-export default Page
+        if(!stateAgencyId) return <div>Not autorized</div>
+      }
+    }
+  }
+  return <div>Agency</div>;
+};
+
+export default Page;
--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -1,9 +1,20 @@
-"use-server";
+"use server";
 
-import { currentUser } from "@clerk/nextjs";
+import { clerkClient, currentUser } from "@clerk/nextjs";
 import { db } from "./db";
 import { redirect } from "next/navigation";
-import { User } from "@prisma/client";
+import {
+  Agency,
+  Lane,
+  Plan,
+  Prisma,
+  Role,
+  SubAccount,
+  Tag,
+  Ticket,
+  User,
+} from '@prisma/client'
+
 
 export const saveActivityLogsNotification = async ({
   agencyId,
@@ -18,20 +29,76 @@ export const saveActivityLogsNotification = async ({
   let userData;
   if (!authUser) {
     const response = await db.user.findFirst({
-      where: { 
-        Agency: { 
-            SubAccount: { 
-                some: { id: subaccountId } } } },
+      where: {
+        Agency: {
+          SubAccount: {
+            some: { id: subaccountId },
+          },
+        },
+      },
     });
-    if(response) {
-        userData = response
+    if (response) {
+      userData = response;
     }
   } else {
-    userData = await db.user.findUnique({ where: {email: authUser?.emailAddresses[0].emailAddress}})
+    userData = await db.user.findUnique({
+      where: { email: authUser?.emailAddresses[0].emailAddress },
+    });
   }
 
-  if(!userData) {
-    throw new Error("Could not find user")
+  if (!userData) {
+    throw new Error("Could not find user");
+  }
+
+  let foundAgencyId = agencyId;
+  if (!foundAgencyId) {
+    if (!subaccountId) {
+      throw new Error(
+        "You need to provide at least agency Id or subaccount Id"
+      );
+    }
+
+    const response = await db.subAccount.findUnique({
+      where: { id: subaccountId },
+    });
+
+    if (response) foundAgencyId = response.agencyId;
+  }
+  if (subaccountId) {
+    await db.notification.create({
+      data: {
+        notification: `${userData.name} | ${description}`,
+        User: {
+          connect: {
+            id: userData.id,
+          },
+        },
+        Agency: {
+          connect: {
+            id: foundAgencyId,
+          },
+        },
+        SubAccount: {
+          connect: { id: subaccountId },
+        },
+      },
+    });
+  } else {
+    await db.notification.create({
+      data: {
+        notification: `${userData.name} | ${description}`,
+        User: {
+          connect: {
+            id: userData.id,
+          },
+        },
+        Agency: {
+          connect: {
+            id: foundAgencyId,
+          },
+        },
+      },
+    });
   }
 };
 
@@ -61,9 +128,34 @@ export const verifyAndAcceptinvitation = async () => {
       createdAt: new Date(),
       updatedAt: new Date(),
     });
-  }
 
-  return invitationExist;
+    await saveActivityLogsNotification({
+      agencyId: invitationExist?.agencyId,
+      description: `Joined`,
+      subaccountId: undefined,
+    });
+
+    if (userDetails) {
+      await clerkClient.users.updateUserMetadata(user.id, {
+        privateMetadata: {
+          role: userDetails.role || "SUBACCOUNT_USER",
+        },
+      });
+
+      await db.invitation.delete({
+        where: { email: userDetails.email },
+      });
+
+      return userDetails.agencyId;
+    } else {
+      return null;
+    }
+  } else {
+    const agency = await db.user.findUnique({
+      where: { email: user.emailAddresses[0].emailAddress },
+    });
+    return agency ? agency.agencyId : null;
+  }
 };
 
 export const getAuthUserDeatils = async () => {
