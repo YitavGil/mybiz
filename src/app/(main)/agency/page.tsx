2098fa9f11859b334da5f51d93df5cfa00fcc7a8
YitavGil <yitavga@yahoo.com>
2024-05-26 17:18:13 +0300

    agency page logic

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -18,19 +18,36 @@ const Page = async ({
     if (user?.role === "SUBACCOUNT_GUEST" || user?.role === "SUBACCOUNT_USER") {
       return redirect("/subaccount");
     } else if (user?.role === "AGENCY_OWNER" || user?.role === "AGENCY_ADMIN") {
-      if(searchParams.plan) {
-        return redirect(`/agency/${agencyId}/billing?plan=${searchParams.plan}`)
+      if (searchParams.plan) {
+        return redirect(
+          `/agency/${agencyId}/billing?plan=${searchParams.plan}`
+        );
       }
 
-      if(searchParams.state){
-        const statePath = searchParams.state.split('___')[0];
-        const stateAgencyId = searchParams.state.split('___')[1];
+      if (searchParams.state) {
+        const statePath = searchParams.state.split("___")[0];
+        const stateAgencyId = searchParams.state.split("___")[1];
 
-        if(!stateAgencyId) return <div>Not autorized</div>
+        if (!stateAgencyId) return <div>Not autorized</div>;
+        return redirect(
+          `/agency/${stateAgencyId}/${statePath}?code=${searchParams.code}`
+        );
+      } else {
+        return redirect(`/agency/${agencyId}`);
       }
+    } else {
+      return <div>Not autorized</div>;
     }
   }
-  return <div>Agency</div>;
+  const authUser = await currentUser();
+  return <div className="flex justify-center items-center mt-4">
+    <div className="max-w-[850px] border  p-4 rounded-xl">
+      <h1 className="text-4xl">Create an agency</h1>
+      <AgencyDetails data={{compantEmail: authUser?.emailAddresses[0].emailAddress}} />
+    </div>
+  </div>
 };
+
+
 
 export default Page;
